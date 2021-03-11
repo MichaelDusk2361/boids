96f3cd021f6372588b38cca8d848c9ef959d0ecc
MichaelDusk2361 <71015888+MichaelDusk2361@users.noreply.github.com>
2021-03-12 00:20:43 +0100

    instantiation in area

--- boids2d/Assets/BoidInstatiator.cs
+++ boids2d/Assets/BoidInstatiator.cs
@@ -5,24 +5,54 @@ using UnityEngine;
 public class BoidInstatiator : MonoBehaviour
 {
     public GameObject boid;
+    public float spawnWidth = 100;
+    public float spawnHeight = 100;
     public int targetInstanceCount = 0;
+
+    private Vector3 topLeft;
+    private Vector3 topRight;
+    private Vector3 bottomLeft;
+    private Vector3 bottomRight;
     private int currentInstanceCount = 0;
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 basePos = gameObject.transform.position;
         for (; currentInstanceCount < targetInstanceCount; currentInstanceCount++)
         {
-            Instantiate(boid, Vector3.zero, Quaternion.identity);
+            float spawnX = Random.Range(basePos.x - spawnWidth / 2, basePos.x + spawnWidth / 2);
+            float spawnY = Random.Range(basePos.y - spawnHeight / 2, basePos.y + spawnHeight / 2);
+            Instantiate(boid, new Vector3(spawnX, spawnY, 0), Quaternion.identity);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(currentInstanceCount < targetInstanceCount)
+        if (currentInstanceCount < targetInstanceCount)
         {
-            Instantiate(boid, Vector3.zero, Quaternion.identity);
+            Vector3 basePos = gameObject.transform.position;
+            float spawnX = Random.Range(basePos.x - spawnWidth / 2, basePos.x + spawnWidth / 2);
+            float spawnY = Random.Range(basePos.y - spawnHeight / 2, basePos.y + spawnHeight / 2);
+            Instantiate(boid, new Vector3(spawnX, spawnY, 0), Quaternion.identity);
             currentInstanceCount++;
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 basePos = gameObject.transform.position;
+        topLeft = new Vector3(basePos.x - spawnWidth / 2, basePos.y + spawnHeight / 2, 0);
+        topRight = new Vector3(basePos.x + spawnWidth / 2, basePos.y + spawnHeight / 2, 0);
+        bottomLeft = new Vector3(basePos.x - spawnWidth / 2, basePos.y - spawnHeight / 2, 0);
+        bottomRight = new Vector3(basePos.x + spawnWidth / 2, basePos.y - spawnHeight / 2, 0);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(topLeft, topRight);
+        Gizmos.DrawLine(topRight, bottomRight);
+        Gizmos.DrawLine(bottomRight, bottomLeft);
+        Gizmos.DrawLine(bottomLeft, topLeft);
+        Gizmos.DrawSphere(basePos, 0.2f);
+    }
 }
+
+
